ee4ba00173a042b3f42b6d1e4c9ea640d9620bbe
Ned Bellavance <ned1313@users.noreply.github.com>
2021-11-11 16:24:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("U+1F32E Hello World! U+1F32E");
 });
 
 const port = process.env.PORT || 1337;
